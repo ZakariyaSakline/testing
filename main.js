344cdd801755ce8b6b1cb6e789a9c9b186924707
Md Zakariya Sakline <zakariya.sakline@selise.com.bd>
2019-02-05 17:02:14 +0600

    add hollo bangladesh

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ var number="";
  for(i=0; i<100; i++){
 number += i +"<br>";
  }
-//console.log(number);
-document.getElementById('text').innerHTML= number;
+console.log(number);
+console.log("hello bangladesh");
+//document.getElementById('text').innerHTML= number;
